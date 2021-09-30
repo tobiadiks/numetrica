4c194deb98ec5b8f8a21e382b5cc5edbd2c07fa0
tobiadiks <tobiadiks@gmail.com>
2021-09-30 20:10:55 +0100

    update

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Footer from "./footer";
 
 export default function Layout(props) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-sans">
       <Header />
 
       <main className="flex-1  px-4 py-6  md:px-6 md:py-12 bg-deep-blue">
