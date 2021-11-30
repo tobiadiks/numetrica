49ebd12ac30d08894179e07a5424120b7ade499f
tobiadiks <tobiadiks@gmail.com>
2021-11-30 16:12:41 +0100

    work on redux signup endpoint

---
+++ pages/signup.js
@@ -0,0 +1,72 @@
+import TextInput from "@components/Inputs/textInput";
+import PrimaryButton from "@components/Inputs/primaryButton";
+import { useState,useEffect} from "react";
+import { useRouter } from "next/router";
+import { login } from "../utils/auth.utils";
+import { useSelector,useDispatch, } from "react-redux";
+
+
+export default function AuthPage() {
+  const route = useRouter();
+  const [business_name, setBusinessName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const userState = useSelector(state=>state.userStore.user)
+  const promiseState = useSelector(state=>state.userStore.status)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(userState.success&&promiseState){
+      route.push('/t/feedback')
+    }
+  },[route,userState.success,promiseState])
+
+
+  const CreateUser = async () => {
+      await dispatch(login({ email, password }));
+      if(!userState.success&&promiseState){
+        await alert('Incorrect credentials')
+      }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-5">
+      <h2 className="p-2 font-bold text-white md:text-2xl text-center w-full md:w-1/2 lg:1/2">
+        Create Your Account
+      </h2>
+{console.log(userState,promiseState)}
+      <section className="flex flex-col items-center justify-center w-full md:w-1/2 lg:1/2">
+      <TextInput
+          value={business_name}
+          onChange={(e) => {
+            setBusinessName(e.target.value                                                                         );
+          }}
+          placeholder="Business Name"
+          type="text"
+        />
+        <TextInput
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value                                                                         );
+          }}
+          placeholder="E-mail"
+          type="text"
+        />
+        <TextInput
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <PrimaryButton onClick={CreateUser} title="Create" />
+
+        <div className="text-led-text flex justify-center align-middle">
+          <div>or</div>&nbsp;
+          <div className="font-semibold underline cursor-pointer">
+            Have an Account? Login
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
