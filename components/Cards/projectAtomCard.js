0ef870799c8a741e950cebc855244418d77de437
tobiadiks <tobiadiks@gmail.com>
2021-12-25 05:03:56 +0100

    up

--- components/Cards/projectAtomCard.js
+++ components/Cards/projectAtomCard.js
@@ -11,67 +11,80 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import cn from "classnames";
 import moment from "moment";
+import UpdateProjectCardComponent from "./updateProject";
 
 export default function ProjectAtomCardComponent(props) {
-    const [drop, setDrop] = useState(false);
-    const [loading, setLoading] = useState(false)
-    const userState = useSelector((state) => state.userStore);
-    const route = useRouter()
-    const DeleteProject = async (project_id) => {
-      setLoading(true);
-      const response = await deleteProject({
-        company_id: userState.user.company.company_id,
-        accessToken: userState.user.accessToken,
-        project_id
-      });
-      console.log(response)
-      if (response.success === false) {
-        await alert("Failed");
-        setLoading(false);
-      } else if (response.success === true) {
+  const [drop, setDrop] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const userState = useSelector((state) => state.userStore);
+  const [hidden, setHidden] = useState(true);
+  const route = useRouter();
+  const DeleteProject = async (project_id) => {
+    setLoading(true);
+    const response = await deleteProject({
+      company_id: userState.user.company.company_id,
+      accessToken: userState.user.accessToken,
+      project_id,
+    });
+    console.log(response);
+    if (response.success === false) {
+      await alert("Failed");
       setLoading(false);
-      route.reload()
-      } else {
-        await alert("Something went wrong!");
-        setLoading(false);
-        
-      }
+    } else if (response.success === true) {
       setLoading(false);
-    };
-  
-    return(
-        <div className={cn("w-full  px-2 py-2 rounded-lg    shadow-sm",props.color)}
-          >
-            <div className="text-white text-sm md:text-xs  font-semibold mb-4 flex justify-between w-full">
-              <span>{moment(props.date).fromNow()}</span>
-              <span className="relative text-white" onClick={() => setDrop(!drop)}>
-                <FontAwesomeIcon icon={drop ? faArrowUp : faArrowDown} />
-                {drop ? (
-                  <ul className="absolute right-1 bg-white text-base md:text-xs   p-2 rounded divide-y w-auto  shadow-2xl ">
-                    <li className="text-gray-800  flex my-1 cursor-pointer">
-                      <div className="mr-1">
-                        <FontAwesomeIcon icon={faEdit} />
-                      </div>
-                      Edit
-                    </li>
-                    <li onClick={()=>{DeleteProject(props.project_id)}} className="text-red-400  flex my-1 cursor-pointer">
-                      <div className="mr-1">
-                        <FontAwesomeIcon icon={faTrash} />
-                      </div>
-                      Delete
-                    </li>
-                  </ul>
-                ) : (
-                  ""
-                )}
-              </span>
-            </div>
-            <div onMouseOver={()=>setDrop(false)} className="text-white mb-4 w-full text-center cursor-pointer">
-              <div className="font-bold text-lg  md:text-medium">{props.name}</div>
-              <div className="text-sm md:text-xs">
-                {props.description}
-              </div>
-            </div>
-          </div>
-    )
+      route.reload();
+    } else {
+      await alert("Something went wrong!");
+      setLoading(false);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div
+      className={cn("w-full  px-2 py-2 rounded-lg    shadow-sm", props.color)}
+    >
+      <UpdateProjectCardComponent
+        hidden={hidden}
+        name={props.name}
+        description={props.description}
+      />
+      <div className="text-white text-sm md:text-xs  font-semibold mb-4 flex justify-between w-full">
+        <span>{moment(props.date).fromNow()}</span>
+        <span className="relative text-white" onClick={() => setDrop(!drop)}>
+          <FontAwesomeIcon icon={drop ? faArrowUp : faArrowDown} />
+          {drop ? (
+            <ul className="absolute right-1 bg-white text-base md:text-xs   p-2 rounded divide-y w-auto  shadow-2xl ">
+              <li onClick={()=>{setHidden(false)}} className="text-gray-800  flex my-1 cursor-pointer">
+                <div className="mr-1">
+                  <FontAwesomeIcon icon={faEdit} />
+                </div>
+                Edit
+              </li>
+              <li
+                onClick={() => {
+                  DeleteProject(props.project_id);
+                }}
+                className="text-red-400  flex my-1 cursor-pointer"
+              >
+                <div className="mr-1">
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+                Delete
+              </li>
+            </ul>
+          ) : (
+            ""
+          )}
+        </span>
+      </div>
+      <div
+        onMouseOver={() => setDrop(false)}
+        className="text-white mb-4 w-full text-center cursor-pointer"
+      >
+        <div className="font-bold text-lg  md:text-medium">{props.name}</div>
+        <div className="text-sm md:text-xs">{props.description}</div>
+      </div>
+    </div>
+  );
 }
---
+++ components/Cards/updateProject.js
@@ -0,0 +1,92 @@
+import TextInput from "@components/Inputs/textInput";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import PrimaryButton from "@components/Inputs/primaryButton";
+import { createProject } from "utils/project.utils";
+import { useRouter } from "next/router";
+
+export default function UpdateProjectCardComponent(props) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [hidden, setHidden] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const userState = useSelector((state) => state.userStore);
+  const route = useRouter()
+  
+
+  const UpdateProject = async () => {
+    
+    setLoading(true);
+    const response = await createProject({
+      company_id: userState.user.company.company_id,
+      accessToken: userState.user.accessToken,
+      name: name,
+      description: description,
+      
+    });
+    console.log(response)
+    if (response.success === false) {
+      await alert("Failed");
+      setLoading(false);
+    } else if (response.success === true) {
+    setHidden(!hidden);
+    setLoading(false);
+    setName("");
+    setDescription("")
+    route.reload()
+    } else {
+      await alert("Something went wrong!");
+      setLoading(false);
+      
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div
+      className={`w-full justify-center align-middle items-center h-full z-50 absolute top-6 backdrop-filter backdrop-blur-sm ${
+        props.hidden == hidden ? "hidden" : ""
+      } `}
+    >{console.log(userState)}
+      <div className="flex w-full md:w-1/3 mx-auto my-auto flex-col items-center justify-center py-4 bg-white shadow-lg">
+        <h2 className="font-bold text-black md:text-sm text-center w-full md:w-1/2 lg:1/2">
+          <div
+            className=" cursor-pointer"
+            onClick={() => {
+              setHidden(!hidden);
+              setLoading(false);
+            }}
+          >
+            <FontAwesomeIcon className=" animate-pulse" icon={faArrowLeft} />
+          </div>
+          Update project
+        </h2>
+
+        <section className="flex flex-col items-center justify-center px-2 md:px-0 w-full md:w-10/12 lg:10/12">
+          <TextInput
+            value={props.name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Name of campaign/project"
+            type="text"
+          />
+          <TextInput
+            value={props.description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description of this project?"
+            type="text"
+          />
+          <PrimaryButton
+            onClick={UpdateProject}
+            loading={loading}
+            title="Add"
+            icon={faPlusCircle}
+          />
+        </section>
+      </div>
+    </div>
+  );
+}
