e14f124feb8027e610881e1e4ca82aae9e0415ee
tobiadiks <tobiadiks@gmail.com>
2022-01-16 22:19:55 +0100

    up

--- pages/c/tool/abtest/[id].js
+++ pages/c/tool/abtest/[id].js
@@ -1,36 +1,55 @@
-import PrimaryButton from "@components/Inputs/primaryButton"
-import Image from "next/image"
+import PrimaryButton from "@components/Inputs/primaryButton";
+import Image from "next/image";
 export default function ABTestPage() {
-    return(
-        <div className="min-h-screen flex flex-col relative md:flex-row md:transform translate-y-1 md:-translate-y-2">
-            <div className="w-full md:w-1/2 flex  align-middle flex-col bg-main-brand3">
-                <h3 className="text-basic1 font-serif-Nunito text-3xl text-center font-bold md:text-7xl md:mt-0 mt-20">A</h3>
-                <div className="px-4 py-4">
-                    <Image
-                    layout="responsive"
-                    alt="A"
-                    src={"/chat.svg"}
-                    height={150}
-                    width={150}
-            />
+  return (
+    <div className="min-h-screen flex flex-col relative md:flex-row md:transform translate-y-1 md:-translate-y-2">
+      <div className="w-full md:w-1/2 flex  align-middle flex-col bg-green-500">
+        <div className="w-24 h-24 backdrop-filter rounded-lg backdrop-brightness-110 md:mt-6 mt-20 mx-auto flex">
+          <div className="bg-white h-[65%] w-full rounded-b-lg self-end flex">
+            <div className="font-bold text-xs text-green-500 text-center mx-auto my-auto">
+              65%
             </div>
-                <div className="w-full px-8 py-8 self-baseline mx-auto my-auto"><PrimaryButton title={"Vote A"}/></div>
-                
-            </div>
-            <div className="h-16 w-16 bg-main-brand1 shadow-main-brand1 text-white flex text-center justify-center  align-middle rounded-full text-2xl absolute left-1/2 top-1/2 md:top-0 md:-translate-y-0 -translate-y-1/2 transform -translate-x-1/2"><div className="mx-auto my-auto font-bold">OR</div></div>
-            <div className="w-full md:w-1/2 flex  align-middle flex-col bg-white">
-            <h3 className="text-main font-serif-Nunito text-basic1 text-3xl text-center font-bold md:text-7xl md:mt-0 mt-20">B</h3>
-            <div className="px-4 py-4">
-                    <Image
-                    layout="responsive"
-                    alt="B"
-                    src={"/critter.svg"}
-                    height={150}
-                    width={150}
-            />
-            </div>
-                <div className="w-full px-8 py-8 self-baseline mx-auto my-auto"><PrimaryButton title={"Vote B"}/></div>
-            </div>
+          </div>
         </div>
-    )
+        <div className="px-4 py-4 bg-white m-12 rounded-lg">
+            <div className=" w-1/6 h-2 rounded-lg bg-green-500 mx-auto my-4"></div>
+            <div className=" w-2/5 h-2 rounded-lg bg-green-200 mx-auto my-2"></div>
+            <div className=" w-1/5 h-2 rounded-lg bg-green-200 mx-auto my-2"></div>
+          <div className="p-4 m-8 bg-green-50 rounded-lg ">
+            <Image
+              layout="responsive"
+              alt="B"
+              src={"/chat.svg"}
+              height={150}
+              width={150}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full md:w-1/2 flex  align-middle flex-col bg-blue-500">
+        <div className="w-24 h-24 backdrop-filter rounded-lg backdrop-brightness-110 md:mt-6 mt-20 mx-auto flex">
+          <div className="bg-white h-[45%] w-full rounded-b-lg self-end flex">
+            <div className="font-bold text-xs text-blue-500 text-center mx-auto my-auto">
+              5%
+            </div>
+          </div>
+        </div>
+        <div className="px-4 py-4 bg-white m-12 rounded-lg">
+            <div className=" w-1/6 h-2 rounded-lg bg-blue-500 mx-auto my-4"></div>
+            <div className=" w-2/5 h-2 rounded-lg bg-blue-200 mx-auto my-2"></div>
+            <div className=" w-1/5 h-2 rounded-lg bg-blue-200 mx-auto my-2"></div>
+          <div className="p-4 m-8 bg-blue-50 rounded-lg ">
+            <Image
+              layout="responsive"
+              alt="B"
+              src={"/critter.svg"}
+              height={150}
+              width={150}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
